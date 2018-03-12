7b92011d4682e07502fb2ecebf36af935692ac31
tomonori takada <tomo@tomonorinoMacBook-Pro.local>
2018-03-12 22:36:15 +0900

    反撃

--- youAndPygmy/battle/battle/AttackOperator.swift
+++ youAndPygmy/battle/battle/AttackOperator.swift
@@ -17,12 +17,36 @@ class AttackOperator{
     }
     //反撃する
     static func counter(aCounterAttacker:BattleChara,aDefender:BattleChara,aEndFunction:@escaping ()->()){
+        let tSkills=aCounterAttacker.getSkill()
+        let tDefenderTrout=Battle.getTrout(aPosition:aDefender.getPosition())!
+        //反撃できるか確認
+        for tSkill in tSkills{
+            if(tSkill==""){continue}
+            let tSkillData=SkillDictionary.get(key:tSkill)
+            if(!tSkillData.counter){continue}//反撃不可スキル
+            if(aCounterAttacker.getCurrentMp()<tSkillData.mp){continue}//mpが足りない
+            let tRange=SkillRangeSearcher.searchSkillRange(aChara:aCounterAttacker,aSkill:tSkill)
+            //攻撃範囲内に反撃する相手がいるかどうか
+            for (tTrout,tInvolvement) in tRange{
+                if(tDefenderTrout !== tTrout){continue}
+                //反撃できる
+                operateSkill(aChara:aCounterAttacker,aSkill:tSkill,aTargetTrout:tTrout,aInvolvement:tInvolvement,
+                             aCounter:false,aEndFunction:{()->()in
+                                //反撃終了後
+                                aEndFunction()
+                })
+                return
+            }
+            continue
+        }
+        //反撃できない
         aEndFunction()
     }
     //スキル効果処理
     static func operateSkill(aChara:BattleChara,aSkill:String,aTargetTrout:BattleTrout,aInvolvement:[BattleTrout],
                               aCounter:Bool,aEndFunction:@escaping ()->()){
         let tSkillData=SkillDictionary.get(key:aSkill)
+        //mp消費
         aChara.useMp(aMp:tSkillData.mp)
         let tTargetChara=aTargetTrout.getRidingChara()!//攻撃目標にされたキャラ
         //スキルを受けるキャラのリスト生成
@@ -38,38 +62,42 @@ class AttackOperator{
             for i in 0..<tDamagedCharas.count{
                 let tDefender=tDamagedCharas[i]
                 let tDamage=DamageCalculator.calculateDamage(aAttacker:aChara,aDefender:tDefender,aSkill:aSkill)!
-                Timer.scheduledTimer(withTimeInterval:0.2*i,repeats:false,block:{(_)->()in
-                    //ダメージのアニメーション
-                    tDefender.addDamage(aDamage:tDamage,aEndFunction:{()->()in
-                        if(i==tDamagedCharas.count-1){
-                            //最後のキャラのダメージアニメーションが終わった
-                            CharaManager.judgeDow()//戦闘不能判定
-                            if(aCounter){
-                                //反撃可能
-                                counter(aCounterAttacker:tTargetChara,aDefender:aChara,
-                                        aEndFunction:aEndFunction)
+                RunLoop.main.add(//メインのrunloopに登録する
+                    Timer.init(timeInterval:0.2*i,repeats:false,block:{(_)->()in
+                        //ダメージのアニメーション
+                        tDefender.addDamage(aDamage:tDamage,aEndFunction:{()->()in
+                            if(i==tDamagedCharas.count-1){
+                                //最後のキャラのダメージアニメーションが終わった
+                                CharaManager.judgeDow()//戦闘不能判定
+                                if(aCounter){
+                                    //反撃可能
+                                    counter(aCounterAttacker:tTargetChara,aDefender:aChara,
+                                            aEndFunction:aEndFunction)
+                                }
+                                else{
+                                    //反撃不可
+                                    aEndFunction()
+                                }
                             }
-                            else{
-                                //反撃不可
-                                aEndFunction()
-                            }
-                        }
+                        })
                     })
-                })
+                    , forMode: RunLoopMode.commonModes)
             }
         case .heal://回復
             for i in 0..<tDamagedCharas.count{
                 let tDefender=tDamagedCharas[i]
                 let tDamage=DamageCalculator.calculateDamage(aAttacker:aChara,aDefender:tDefender,aSkill:aSkill)!
-                Timer.scheduledTimer(withTimeInterval:0.2*i,repeats:false,block:{(_)->()in
-                    //ダメージのアニメーション
-                    tDefender.heal(aHeal:tDamage,aEndFunction:{()->()in
-                        if(i==tDamagedCharas.count-1){
-                            //最後のキャラのダメージアニメーションが終わった
-                            aEndFunction()
-                        }
+                RunLoop.main.add(//メインのrunloopに登録する
+                    Timer.init(timeInterval:0.2*i,repeats:false,block:{(_)->()in
+                        //ダメージのアニメーション
+                        tDefender.heal(aHeal:tDamage,aEndFunction:{()->()in
+                            if(i==tDamagedCharas.count-1){
+                                //最後のキャラのダメージアニメーションが終わった
+                                aEndFunction()
+                            }
+                        })
                     })
-                })
+                    , forMode: RunLoopMode.commonModes)
             }
 //        case .assist://支援
 //        case .disturbance://妨害
